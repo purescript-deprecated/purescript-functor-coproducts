679377f76d0f10aa6c69443ee84a6f4f5d521f7e
Gary Burgess <gary.burgess@gmail.com>
2016-10-09 22:06:28 +0100

    Prepare for 2.0 release

--- src/Data/Functor/Coproduct.purs
+++ src/Data/Functor/Coproduct.purs
@@ -3,16 +3,13 @@ module Data.Functor.Coproduct where
 import Prelude
 
 import Data.Bifunctor (bimap)
-import Data.Either (Either(..), either)
+import Data.Either (Either(..))
 import Data.Foldable (class Foldable, foldMap, foldl, foldr)
+import Data.Newtype (class Newtype)
 import Data.Traversable (class Traversable, traverse, sequence)
 
 -- | `Coproduct f g` is the coproduct of two functors `f` and `g`
 newtype Coproduct f g a = Coproduct (Either (f a) (g a))
-
--- | Unwrap a coproduct
-unCoproduct :: forall f g a. Coproduct f g a -> Either (f a) (g a)
-unCoproduct (Coproduct x) = x
 
 -- | Left injection
 left :: forall f g a. f a -> Coproduct f g a
@@ -25,7 +22,8 @@ right ga = Coproduct (Right ga)
 -- | Eliminate a coproduct by providing eliminators for the left and
 -- | right components
 coproduct :: forall f g a b. (f a -> b) -> (g a -> b) -> Coproduct f g a -> b
-coproduct f g (Coproduct e) = either f g e
+coproduct f _ (Coproduct (Left a)) = f a
+coproduct _ g (Coproduct (Right b)) = g b
 
 -- | Change the underlying functors in a coproduct
 bihoistCoproduct
@@ -35,6 +33,16 @@ bihoistCoproduct
   -> Coproduct f g
   ~> Coproduct h i
 bihoistCoproduct natF natG (Coproduct e) = Coproduct (bimap natF natG e)
+
+derive instance newtypeCoproduct :: Newtype (Coproduct f g a) _
+
+derive instance eqCoproduct :: (Eq (f a), Eq (g a)) => Eq (Coproduct f g a)
+
+derive instance ordCoproduct :: (Ord (f a), Ord (g a)) => Ord (Coproduct f g a)
+
+instance showCoproduct :: (Show (f a), Show (g a)) => Show (Coproduct f g a) where
+  show (Coproduct (Left fa)) = "(left " <> show fa <> ")"
+  show (Coproduct (Right ga)) = "(right " <> show ga <> ")"
 
 instance functorCoproduct :: (Functor f, Functor g) => Functor (Coproduct f g) where
   map f (Coproduct e) = Coproduct (bimap (map f) (map f) e)
